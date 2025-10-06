44738a8313f16345d7ba4e17091fc07be5e70d32
nojia1775 <nojia.adjemrose@gmail.com>
2025-10-06 17:37:54 +0200

    prenez moi en stage svp pitie je fais le cafe

--- neural_network/src/ARNetwork.cpp
+++ neural_network/src/ARNetwork.cpp
@@ -66,25 +66,25 @@ void	ARNetwork::set_bias(const size_t& i, const size_t& j, const double& bias)
 
 Vector<double>	ARNetwork::feed_forward(const Vector<double>& inputs, double (*layer_activation)(const double&), double (*output_activation)(const double&))
 {
-	std::cout << "FRONT\n";
+	// std::cout << "FRONT\n";
 	set_inputs(inputs);
 	Matrix<double> neurals = _inputs;
-	std::cout << "INPUTS : \n";
-	neurals.display();
-	std::cout << std::endl;
+	// std::cout << "INPUTS : \n";
+	// neurals.display();
+	// std::cout << std::endl;
 	_a[0] = _inputs;
 	for (size_t i = 0 ; i < nbr_hidden_layers() + 1 ; i++)
 	{
-		std::cout << "WEIGHTS " << i << std::endl;
-		_weights[i].display();
-		std::cout << std::endl;
-		std::cout << "BIAS " << i << std::endl;
-		_bias[i].display();
-		std::cout << std::endl;
+		// std::cout << "WEIGHTS " << i << std::endl;
+		// _weights[i].display();
+		// std::cout << std::endl;
+		// std::cout << "BIAS " << i << std::endl;
+		// _bias[i].display();
+		// std::cout << std::endl;
 		_z[i] = _weights[i] * neurals + Matrix<double>(_bias[i]);
-		std::cout << "Z " << i << std::endl;
-		_z[i].display();
-		std::cout << std::endl;
+		// std::cout << "Z " << i << std::endl;
+		// _z[i].display();
+		// std::cout << std::endl;
 		neurals = _z[i];
 		if (i == nbr_hidden_layers())
 		{
@@ -96,9 +96,9 @@ Vector<double>	ARNetwork::feed_forward(const Vector<double>& inputs, double (*la
 			if (layer_activation)
 				neurals = neurals.apply(layer_activation);
 		}
-		std::cout << "A " << i << std::endl;
-		neurals.display();
-		std::cout << std::endl;
+		// std::cout << "A " << i << std::endl;
+		// neurals.display();
+		// std::cout << std::endl;
 		if (i != nbr_hidden_layers())
 			_a[i + 1] = neurals;
 	}
@@ -108,53 +108,56 @@ Vector<double>	ARNetwork::feed_forward(const Vector<double>& inputs, double (*la
 
 void	ARNetwork::back_propagation(std::vector<Matrix<double>>& dW, std::vector<Matrix<double>>& dZ, const PairFunction& loss_functions, double (*d_layer_activation)(const double&), double (*d_output_activation)(const double&), const Vector<double>& y)
 {
-	std::cout << "\nBACK\n";
+	// std::cout << "\nBACK\n";
 	Matrix<double> dA(loss_functions.derived_foo(_outputs, y));
 	for (int l = nbr_hidden_layers() ; l >= 0 ; l--)
 	{
-		std::cout << "Z " << l << std::endl;
-		_z[l].display();
-		std::cout << std::endl;
-		std::cout << "Z apply " << l << std::endl;
-		_z[l].apply(l == (int)nbr_hidden_layers() ? d_output_activation : d_layer_activation).display();
-		std::cout << std::endl;
-		std::cout << "dA " << l << std::endl;
-		dA.display();
-		std::cout << std::endl;
-		Matrix<double> z(dA.hadamard(_z[l].apply(l == (int)nbr_hidden_layers() ? d_output_activation : d_layer_activation)));
-		std::cout << "A transpose " << l << std::endl;
-		Matrix<double>(_a[l]).transpose().display();
-		std::cout << std::endl;
-		std::cout << "dZ " << l << std::endl;
-		z.display();
-		std::cout << std::endl;
-		Matrix<double> w(z * Matrix<double>(_a[l]).transpose());
-		std::cout << "dW " << l << std::endl;
-		w.display();
-		std::cout << std::endl;
+		// std::cout << "Z " << l << std::endl;
+		// _z[l].display();
+		// std::cout << std::endl;
+		// std::cout << "Z apply " << l << std::endl;
+		// _z[l].apply(l == (int)nbr_hidden_layers() ? d_output_activation : d_layer_activation).display();
+		// std::cout << std::endl;
+		// std::cout << "dA " << l << std::endl;
+		// dA.display();
+		// std::cout << std::endl;
+		Matrix<double> z = dA.hadamard(_z[l].apply(l == (int)nbr_hidden_layers() ? d_output_activation : d_layer_activation));
+		// std::cout << "A transpose " << l << std::endl;
+		// Matrix<double>(_a[l]).transpose().display();
+		// std::cout << std::endl;
+		// std::cout << "dZ " << l << std::endl;
+		// z.display();
+		// std::cout << std::endl;
+		Matrix<double> w = z * Matrix<double>(_a[l]).transpose();
+		// std::cout << "dW " << l << std::endl;
+		// w.display();
+		// std::cout << std::endl;
 		dZ[l] = dZ[l] + z;
 		dW[l] = dW[l] + w;
+		// std::cout << "WEIGHTS TRANSPOSE " << l << std::endl;
+		// _weights[l].transpose().display();
+		// std::cout << std::endl;
 		dA = _weights[l].transpose() * z;
 	}
 }
 
 void	ARNetwork::update_weights_bias(const std::vector<Matrix<double>>& dW, const std::vector<Matrix<double>>& dZ, const size_t& batch)
 {
-	std::cout << "\nUPDATE\n";
+	// std::cout << "\nUPDATE\n";
 	for (size_t layer = 0 ; layer < nbr_hidden_layers() + 1 ; layer++)
 	{
-		std::cout << "CURRENT WEIGHTS " << layer << std::endl;
-		_weights[layer].display();
-		std::cout << "dW" << layer << std::endl;
-		dW[layer].display();
-		std::cout << "CURRENT BIAS " << layer << std::endl;
-		_bias[layer].display();
-		std::cout << "dZ " << layer << std::endl;
-		dZ[layer].display();
-		std::cout << "LEARNING RATE " << _learning_rate << std::endl;
-		std::cout << "1 / BATCH " << 1 / batch << std::endl;
-		_weights[layer] = _weights[layer] - dW[layer] * _learning_rate * (1 / batch);
-		_bias[layer] = Matrix<double>(_bias[layer]) - dZ[layer] * _learning_rate * (1 / batch);
+		// std::cout << "CURRENT WEIGHTS " << layer << std::endl;
+		// _weights[layer].display();
+		// std::cout << "dW" << layer << std::endl;
+		// dW[layer].display();
+		// std::cout << "CURRENT BIAS " << layer << std::endl;
+		// _bias[layer].display();
+		// std::cout << "dZ " << layer << std::endl;
+		// dZ[layer].display();
+		// std::cout << "LEARNING RATE " << _learning_rate << std::endl;
+		// std::cout << "1 / BATCH " << 1 / batch << std::endl;
+		_weights[layer] = _weights[layer] - dW[layer] * _learning_rate * static_cast<double>(1.0 / static_cast<double>(batch));
+		_bias[layer] = Matrix<double>(_bias[layer]) - dZ[layer] * _learning_rate * static_cast<double>(1.0 / static_cast<double>(batch));
 	}
 }
 
@@ -194,9 +197,8 @@ std::vector<double>	ARNetwork::train(const PairFunction& loss_functions, const P
 			for (size_t k = 0 ; k < inputs[j].size() ; k++) // batch of examples
 			{
 				Vector<double> prediction = feed_forward(inputs[j][k], layer_functions.get_activation_function(), output_functions.get_activation_function());
-				for (size_t l = 0 ; l < prediction.dimension() ; l++)
-					loss_index += loss_functions.foo(prediction, outputs[j][k]);
-				back_propagation(dW, dZ, loss_functions, layer_functions.get_derived_activation_function(), output_functions.get_derived_activation_function(), prediction);
+				loss_index = loss_functions.foo(prediction, outputs[j][k]) / prediction.dimension();
+				back_propagation(dW, dZ, loss_functions, layer_functions.get_derived_activation_function(), output_functions.get_derived_activation_function(), outputs[j][k]);
 			}
 			losses.push_back(loss_index / inputs[j].size());
 			update_weights_bias(dW, dZ, inputs[j].size());
